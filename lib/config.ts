4255b53d631a25f61edae533db6bc77769bc8a97
reinexworldc <rydanh400@gmail.com>
2025-05-15 18:05:08 +0300

    nginx added

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,4 @@
 // Конфигурация API URL
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || '/api';
 
 export { API_URL }; 
